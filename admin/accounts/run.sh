c7855ccc5ce88002922dad7a0e911db32b5df470
Michael Gibney <michaelgibney1@gmail.com>
2022-07-26 18:46:24 -0700

    added pip jq install

--- admin/accounts/run.sh
+++ admin/accounts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ansible-playbook accountsPlaybook.yaml
+pip install jq
+ansible-playbook ./ansible/accountsPlaybook.yaml
 # ansible-playbook -i vyos.example.net, -u ansible -k -e ansible_network_os=vyos.vyos.vyos first_playbook.yml
